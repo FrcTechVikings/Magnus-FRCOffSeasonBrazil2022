dd46f2abb363ff1f71c808f8e9bf5cf632289345
FrcTechVikings <frctechvikings@gmail.com>
2022-08-12 16:30:20 -0400

    Post Season Commit

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -2,22 +2,15 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//TODO: Organizar melhor a localização do autônomo do robô -> Já sei, utilizar o mesmo CommandGroup, separando as funções em autônomo e teleop, criando um arquivo para cada
+
 #include "Robot.h"
 
 void Robot::RobotInit() {
 
   // Adiciona as opções de autônomo
-  m_chooser.SetDefaultOption(AutoConstants::OneCargoAutoVariableDist, AutoConstants::OneCargoAutoVariableDist);
-
-  m_chooser.AddOption(AutoConstants::OneCargoAutoOption, AutoConstants::OneCargoAutoOption);
-  m_chooser.AddOption(AutoConstants::ExitTarmacDelayOption, AutoConstants::ExitTarmacDelayOption);
-  m_chooser.AddOption(AutoConstants::ExitTarmacOption, AutoConstants::ExitTarmacOption);
-  m_chooser.AddOption(AutoConstants::OneCargoAutoDelayOption, AutoConstants::OneCargoAutoDelayOption);
-  m_chooser.AddOption(AutoConstants::OneCargoAutoDelayWalkOption, AutoConstants::OneCargoAutoDelayWalkOption);
-
-  m_chooser.AddOption(AutoConstants::OneCargoAutoWalkOption, AutoConstants::OneCargoAutoWalkOption);
-  m_chooser.AddOption(AutoConstants::OneCargoAutoMoreOption, AutoConstants::OneCargoAutoMoreOption);
-  m_chooser.AddOption(AutoConstants::OneCargoAutoLessOption, AutoConstants::OneCargoAutoLessOption);
+  m_chooser.SetDefaultOption(AutoConstants::oneCargoAutoVariableDist, AutoConstants::oneCargoAutoVariableDist);
+  m_chooser.AddOption(AutoConstants::nothingAuto, AutoConstants::nothingAuto);
 
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
@@ -40,17 +33,7 @@ void Robot::AutonomousInit() {
 void Robot::AutonomousPeriodic() {
 
   // Executa o autônomo relacionado ao valor escolhido na driver station
-  if (m_autoSelected == AutoConstants::ExitTarmacOption){RobotCommands.ExitTarmacAuto(0.0);
-  }else if(m_autoSelected == AutoConstants::ExitTarmacDelayOption){RobotCommands.ExitTarmacAuto(AutoConstants::ExitTarmac::delayExitTarmacAuto); // Estamos com delay de 7 segundos 
-  }else if(m_autoSelected == AutoConstants::OneCargoAutoDelayOption){RobotCommands.OneCargoAuto(AutoConstants::OneCargoAuto::delayOneCargoAuto, 0.0, AutoConstants::OneCargoAuto::exitingTarmacDistance); // Estamos com delay de 7 segundos
-  }else if(m_autoSelected == AutoConstants::OneCargoAutoWalkOption){RobotCommands.OneCargoAuto(0.0, AutoConstants::OneCargoAuto::slightlyWalkForwardDistance, AutoConstants::OneCargoAuto::exitingTarmacDistance);
-  }else if(m_autoSelected == AutoConstants::OneCargoAutoDelayWalkOption){RobotCommands.OneCargoAuto(AutoConstants::OneCargoAuto::delayOneCargoAuto, AutoConstants::OneCargoAuto::slightlyWalkForwardDistance, AutoConstants::OneCargoAuto::exitingTarmacDistance); // Estamos com delay de 7 segundos
-
-  }else if(m_autoSelected == AutoConstants::OneCargoAutoOption){RobotCommands.OneCargoAuto(0.0, 0.0, AutoConstants::OneCargoAuto::exitingTarmacDistance);
-  }else if(m_autoSelected == AutoConstants::OneCargoAutoMoreOption){RobotCommands.OneCargoAuto(0.0, 0.0, AutoConstants::OneCargoAuto::exitingTarmacDistanceMore);
-  }else if(m_autoSelected == AutoConstants::OneCargoAutoLessOption){RobotCommands.OneCargoAuto(0.0, 0.0, AutoConstants::OneCargoAuto::exitingTarmacDistanceLess);
-  
-  }else if(m_autoSelected == AutoConstants::OneCargoAutoVariableDist){RobotCommands.OneCargoAuto(frc::SmartDashboard::GetNumber("Tempo", 0.0), frc::SmartDashboard::GetNumber("Aproximar", 0.5), frc::SmartDashboard::GetNumber("SairTarmac", 2.0));}
+  if(m_autoSelected == AutoConstants::oneCargoAutoVariableDist){RobotCommands.OneCargoAuto(frc::SmartDashboard::GetNumber("Tempo", 0.0), frc::SmartDashboard::GetNumber("Aproximar", 0.5), frc::SmartDashboard::GetNumber("SairTarmac", 2.0));}
 
 }
 
--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -2,26 +2,17 @@
 
 namespace AutoConstants{
 
-    const std::string ExitTarmacOption = "Sair Tarmac 2 metros";
-    const std::string ExitTarmacDelayOption = "Sair Tarmac Com Delay 2 metros";
-    const std::string OneCargoAutoDelayOption = "Uma Cargo Com Delay e 2 metros";
-    const std::string OneCargoAutoDelayWalkOption = "Uma Cargo com Delay, Movimentação e 2 metros";
-    const std::string OneCargoAutoWalkOption = "Uma Cargo com Movimentação e 2 metros";
-
-    const std::string OneCargoAutoOption = "Uma Cargo e 2 metros";
-    const std::string OneCargoAutoMoreOption = "Uma Cargo e 2.2 metros";
-    const std::string OneCargoAutoLessOption = "Uma Cargo e 1.8 metros";
-
-    const std::string OneCargoAutoVariableDist = "Uma Cargo e distância variável";
+    const std::string oneCargoAutoVariableDist = "Uma Cargo e distância variável";
+    const std::string nothingAuto = "Nada";
 
     namespace OneCargoAuto{
 
         const double delayOneCargoAuto = 6.0; // Seconds ////////// Definir ainda!
 
-        const double robotStartConfigTime = 0.4; // Seconds
-        const double slightlyWalkForwardTime = 1.6; // Seconds
-        const double intakeShootTime = 2.8; // Seconds
-        const double exitingTarmacTime = 6.3; // Seconds
+        const double robotStartConfigTime = 0.2; // Seconds
+        const double slightlyWalkForwardTime = 1.3; // Seconds
+        const double intakeShootTime = 2.6; // Seconds
+        const double exitingTarmacTime = 6.1; // Seconds
 
         const double slightlyWalkForwardDistance = 0.35; // Meters
 
@@ -35,10 +26,10 @@ namespace AutoConstants{
 
         const double delayExitTarmacAuto = 7.0; // Seconds ////////// Definir ainda!
 
-        const double robotStartConfigTime = 0.4; // Seconds
-        const double exitingTarmacTime = 6.4; // Seconds
+        const double robotStartConfigTime = 0.2; // Seconds
+        const double exitingTarmacTime = 6.2; // Seconds
 
-        const double exitingTarmacDistance = 2.0; // Meters
+        const double exitingTarmacDistance = 1.8; // Meters
 
     }
 
@@ -46,64 +37,64 @@ namespace AutoConstants{
 
 namespace DriveTrainConstants {
 
-    const double percentGain = 0.1;
-    const double maxSpeedPercentConstrain = 1.0;
-    const double minSpeedPercentConstrain = 0.2;
-    const double maxRotationPercentConstrain = 0.85;
-    const double minRotationPercentConstrain = 0.05;
-    const double standardPercentSpeed = 0.6;
-    const double standardPercentRotation = 0.45;
+    const double percentGain = 0.1; // Percentage
+    const double maxSpeedPercentConstrain = 1.0; // Percentage
+    const double minSpeedPercentConstrain = 0.2; // Percentage
+    const double maxRotationPercentConstrain = 0.85; // Percentage
+    const double minRotationPercentConstrain = 0.05; // Percentage
+    const double standardPercentSpeed = 0.6; // Percentage
+    const double standardPercentRotation = 0.45; // Percentage
 
 }
 
 namespace RobotConstants {
 
-    const double wheelLenghtMeters = 0.4787;
-    const double wheelRadiusMeters = 0.0762;
+    const double wheelLenghtMeters = 0.4787; // Meters
+    const double wheelRadiusMeters = 0.0762; // Meters
 
 }
 
 namespace IntakeConstants {
 
-    const double percentIntakeCollect = 1.0;
-    const double percentIntakeStandardRelease = 0.7;
-    const double maxPercentIntakeRelease = 1.0;
-    const double minPercentIntakeRelease = 0.5;
-    const double percentGain = 0.1;
+    const double percentIntakeCollect = 1.0; // Percentage
+    const double percentIntakeStandardRelease = 0.7; // Percentage
+    const double maxPercentIntakeRelease = 1.0; // Percentage
+    const double minPercentIntakeRelease = 0.5; // Percentage
+    const double percentGain = 0.1; // Percentage
 
 }
 
 namespace ArmConstants {
 
-    const double armPercentConstrain = 0.8;
-    const double armKeepUp = 0.05;
-    const double holderPercent = 0.4;
-    const double operatorArmDeadBand = 0.5;
-    const double armPercentUp = 0.8;
-    const double armPercentDown = -0.7;
+    const double armPercentConstrain = 0.8; // Percentage
+    const double armKeepUp = 0.05; // Percentage
+    const double holderPercent = 0.4; // Percentage
+    const double operatorArmDeadBand = 0.5; // Percentage
+    const double armPercentUp = 0.8; // Percentage
+    const double armPercentDown = -0.7; // Percentage
 
 }
 
 namespace MotorsConstants {
 
-    const int rearLeftCIN = 0;
-    const int frontLeftCIN = 1;
-    const int rearRightCIN = 2;
-    const int frontRightCIN = 3;
-    const int armRedline = 4;
-    const int intakeRedline = 5;
-    const int armSnowBlower = 6;
+    const int rearLeftCIN = 0; // ID
+    const int frontLeftCIN = 1; // ID
+    const int rearRightCIN = 2; // ID
+    const int frontRightCIN = 3; // ID
+    const int armRedline = 4; // ID
+    const int intakeRedline = 5; // ID
+    const int armSnowBlower = 6; // ID
 
 }
 
 namespace SensorsConstants {
 
-    const int leftDriveEncoderAChannel = 0;
-    const int leftDriveEncoderBChannel = 1;
-    const int rightDriveEncoderAChannel = 2;
-    const int rightDriveEncoderBChannel = 3;
-    const int bottomMicroSwitchDIO = 4;
-    const int topMicroSwitchDIO = 5;
+    const int leftDriveEncoderAChannel = 0; // ID
+    const int leftDriveEncoderBChannel = 1; // ID
+    const int rightDriveEncoderAChannel = 2; // ID
+    const int rightDriveEncoderBChannel = 3; // ID
+    const int bottomMicroSwitchDIO = 4; // ID
+    const int topMicroSwitchDIO = 5; // ID
 
 }
 
@@ -111,8 +102,8 @@ namespace JoystickConstants {
 
     // Removido por agora
 
-    //const double yStickBand = 0.1;
-    //const double zStickBand = 0.0;
+    //const double yStickBand = 0.1; // Percentage
+    //const double zStickBand = 0.0; // Percentage
 
     const int buttonX = 1;
     const int buttonA = 2;
